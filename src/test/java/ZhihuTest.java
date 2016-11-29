aa96770666b0e1607055c2425c193657e76c879c
MatrixSeven <hacker.kill07@gmail.com>
2016-11-29 20:47:55 +0800

    增加测试类，资源文件

---
+++ src/test/java/ZhihuTest.java
@@ -0,0 +1,32 @@
+//=======================================================
+//		          .----.
+//		       _.'__    `.
+//		   .--(^)(^^)---/!\
+//		 .' @          /!!!\
+//		 :         ,    !!!!
+//		  `-..__.-' _.-\!!!/
+//		        `;_:    `"'
+//		      .'"""""`.
+//		     /,  ya ,\\
+//		    //π∑…Ò±£””\\
+//		    `-._______.-'
+//		    ___`. | .'___
+//		   (______|______)
+//=======================================================
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+/**
+ * [Zhihu]https://www.zhihu.com/people/Sweets07
+ * [Github]https://github.com/MatrixSeven
+ * Created by seven on 2016/11/29.
+ */
+@RunWith(BlockJUnit4ClassRunner.class)
+public class ZhihuTest {
+    @Test
+    public void Test_01(){
+
+    }
+}
